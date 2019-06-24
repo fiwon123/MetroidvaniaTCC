88e1630cf49c600c39ca63b794997607b8dd8b9f
fiwon123 <felipeinoue@yahoo.com.br>
2019-06-24 18:21:38 -0300

    Start dialgoue system

--- MetroidvaniaTCC/Scripts/Dialogue.gd
+++ MetroidvaniaTCC/Scripts/Dialogue.gd
@@ -1,29 +1,34 @@
 extends Polygon2D
 
-var dialog = ["Herói: O que está acontecendo...", 
-				"Herói: Não me lembro de nada...", 
-				"Herói: Este lugar é escuro e estou apenas com minha espada"]
+var dialogue = []
+
 var page = 0
 
 var is_finish = false
 
 onready var text = $RichTextLabel
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	Global.dialogue = self
-	text.set_bbcode(dialog[page])
+
+func start(dialogue):
+	self.dialogue = dialogue
+	self.visible = true
+	page = 0
 	text.visible_characters = 0
+	text.set_bbcode(tr(dialogue[page]))
+	$Timer.start()
 
 func _input(event):
 	if event is InputEventMouseButton && event.is_pressed():
 		if text.visible_characters > text.get_total_character_count() and not is_finish:
-			if page < dialog.size()-1:
+			if page < dialogue.size()-1:
 				page += 1
-				text.set_bbcode(dialog[page])
+				text.set_bbcode(tr(dialogue[page]))
 				text.visible_characters = 0
 			else:
 				is_finish = true
+				self.visible = false
 				Global.game.dialog_finish()
 
 func _on_Timer_timeout():
