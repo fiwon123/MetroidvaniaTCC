ed80a8685a8f6f2b55745b798663492623727c93
fiwon123 <felipeinoue@yahoo.com.br>
2019-06-25 01:23:07 -0300

    ScreenBase Completed

--- MetroidvaniaTCC/Scripts/Global.gd
+++ MetroidvaniaTCC/Scripts/Global.gd
@@ -4,3 +4,10 @@ var dialogue
 var GUI
 var Camera2D
 var game
+
+#Scenes
+var MAIN_MENU = "res://Scenes/MainMenu.tscn"
+var OPTIONS = "res://Scenes/Options.tscn"
+var CREDITS = "res://Scenes/Credits.tscn"
+var SAVES = "res://Scenes/Saves.tscn"
+var GAME = "res://Scenes/Levels/Level1.tscn"
