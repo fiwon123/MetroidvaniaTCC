a6f9271e7060b6a2a954dc9c0b67feefd72df61a
fiwon123 <felipeinoue@yahoo.com.br>
2019-08-14 17:26:59 -0300

    Complete base map

---
+++ MetroidvaniaTCC/Scripts/OldMan.gd
@@ -0,0 +1,21 @@
+extends "res://Scripts/Trigger.gd"
+
+func _ready():
+	dialogue = [["forest-2-1", "forest-2-2", "forest-2-3", "forest-2-4"]]
+
+
+
+func _on_Letter_body_entered(body):
+	var tween = get_node("Tween")
+	tween.interpolate_property($buttonZ, "modulate",
+        $buttonZ.modulate, Color(1, 1, 1, 1), 0.5,
+        Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+	tween.start()
+
+
+func _on_Letter_body_exited(body):
+	var tween = get_node("Tween")
+	tween.interpolate_property($buttonZ, "modulate",
+        $buttonZ.modulate, Color(1, 1, 1, 0), 0.5,
+        Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+	tween.start()
